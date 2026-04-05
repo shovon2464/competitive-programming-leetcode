c2274bdf9262157d8f1b390b756d5418813b5665
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-04-05 13:27:20 -0600

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<List<Integer>> adjacencyList;
+    Set<Integer> visited;
+    public boolean dfs(int node, int preNode) {
+        if (visited.contains(node)) return false;
+        visited.add(node);
+        for (int neighbor : adjacencyList.get(node)) {
+            if (neighbor == preNode) continue;
+            if (!dfs(neighbor, node)) return false;
+        }
+        return true;
+    }
+    public boolean validTree(int n, int[][] edges) {
+        this.adjacencyList = new ArrayList<>();
+        this.visited = new HashSet<>();
+        for(int i = 0; i < n; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
+        }
+
+        return dfs(0, -1) && visited.size() == n;
+    }
+}
