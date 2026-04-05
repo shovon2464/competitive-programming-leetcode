aa830b265b9db7228afe469df61533ef93c3bf8d
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-04-05 17:21:52 -0600

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.java
@@ -0,0 +1,42 @@
+class Solution {
+    int[] parents;
+    int[] rank;
+    public int find(int node) {
+        int res = node;
+        while (res != parents[res]) {
+            res = parents[res];
+        }
+        return res;
+    }
+    public int union(int node1, int node2) {
+        int parent1 = find(node1);
+        int parent2 = find(node2);
+
+        if (parent1 == parent2) return 0;
+
+        if(rank[parent1] > rank[parent2]) {
+            parents[node2] = node1;
+            rank[node1] += rank[node2];
+        } else {
+            parents[node1] = node2;
+            rank[node2] += rank[node1];
+        }
+        return 1;
+    }
+    public int countComponents(int n, int[][] edges) {
+        this.parents = new int[n];
+        this.rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+            rank[i] = 1;
+        }
+
+        int result = n;
+        for (int[] edge : edges) {
+            result -= union(edge[0], edge[1]);
+        }
+
+        return result;
+    }
+}
