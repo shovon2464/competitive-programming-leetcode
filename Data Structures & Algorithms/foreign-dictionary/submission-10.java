ac62f6ca0d1c4ca91fde1daac125f32a77e02218
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-05-05 01:29:10 -0600

    Add: foreign-dictionary - submission-10

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-10.java
@@ -0,0 +1,58 @@
+class Solution {
+    Map<Character, Set<Character>> adj;
+    Map<Character, Boolean> visited;
+    StringBuilder result;
+
+    public boolean dfs(Character node) {
+      if (visited.containsKey(node)) {
+        return visited.get(node);
+      }
+
+      visited.put(node, true);
+
+      for (Character neighbor : adj.get(node)) {
+        if (dfs(neighbor)) return true;
+      }
+
+      result.append(node);
+      visited.put(node, false);
+      return false;
+    }
+    public String foreignDictionary(String[] words) {
+      int N = words.length;
+      this.adj = new HashMap<>();
+      this.visited = new HashMap<>();
+      this.result = new StringBuilder();
+
+      for (String word : words) {
+        for (char c : word.toCharArray()) {
+          adj.put(c, new HashSet<>());
+        }
+      }
+
+      for (int i = 0; i < N - 1; i ++) {
+        String word1 = words[i];
+        String word2 = words[i+1];
+
+        if (word1.length() > word2.length() && word1.startsWith(word2)) {
+          return "";
+        }
+        int minLen = Math.min(word1.length(), word2.length());
+
+        for (int j = 0; j < minLen; j++) {
+          if (word1.charAt(j) != word2.charAt(j)) {
+            adj.get(word1.charAt(j)).add(word2.charAt(j));
+            break;
+          }
+        }
+      }
+
+      for (Character key : adj.keySet()) {
+        if(dfs(key)) return "";
+      }
+
+      result.reverse();
+
+      return result.toString();
+    }
+}
