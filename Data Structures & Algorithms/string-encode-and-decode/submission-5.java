74114d3c18035cc5ee35b97ad3cdf4cb015bee8b
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-05-06 18:58:20 -0600

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String str : strs) {
+            sb.append(str.length());
+            sb.append('#');
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+
+        while (i < str.length()) {
+            if (Character.isDigit(str.charAt(i))) {
+                sb.append(str.charAt(i));
+                i++;
+            } else if (str.charAt(i) == '#') {
+                int size = Integer.parseInt(sb.toString());
+                sb.setLength(0);
+                i ++;
+                for (int j = 0; j < size; j++) {
+                    sb.append(str.charAt(i));
+                    i++;
+                }
+                result.add(sb.toString());
+                sb.setLength(0);
+            }
+        }
+
+        return result;
+    }
+}
