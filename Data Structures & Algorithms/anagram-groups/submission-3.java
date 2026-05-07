c148c1c03aff001772f253f3e88f91069aa09967
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-05-06 18:27:17 -0600

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            int[] tracker = new int[26];
+            for (int i = 0; i < str.length(); i++) {
+                int index = (int) str.charAt(i) - 'a';
+                tracker[index] ++;
+            }
+            String key = Arrays.toString(tracker);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
