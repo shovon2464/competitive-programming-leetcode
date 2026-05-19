309176a07add0dab8e8d40494d1d128ae51c7a52
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-05-18 18:16:43 -0600

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = 1;
+        int right = x;
+
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int target = x / mid;
+
+            if (mid <= target) {
+                left = mid + 1;
+                answer = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+}
