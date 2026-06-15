a947fd7d7169ddb250595dc8876a40bd2e85aabf
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-06-15 14:06:04 -0600

    Add: house-robber-iii - submission-1

---
+++ Data Structures & Algorithms/house-robber-iii/submission-1.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] ans = dfs(root);
+        return Math.max(ans[0], ans[1]);
+    }
+
+    public int[] dfs(TreeNode root) {
+        if (root == null) return new int[] {0,0};
+        int[] leftPair = dfs(root.left);
+        int[] rightPair = dfs(root.right);
+        
+        int withRoot = root.val + leftPair[1] + rightPair[1];
+        int withoutRoot = Math.max(leftPair[0], leftPair[1]) + Math.max(rightPair[0], rightPair[1]);
+        return new int[] {withRoot, withoutRoot};
+    }
+}
