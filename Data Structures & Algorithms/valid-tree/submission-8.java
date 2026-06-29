a35d04634a95de9d5f90eeb03ecd1f61e9ce893d
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-06-29 15:44:58 -0600

    Add: valid-tree - submission-8

---
+++ Data Structures & Algorithms/valid-tree/submission-8.java
@@ -0,0 +1,29 @@
+class Solution {
+    Map<Integer, List<Integer>> adj;
+    Set<Integer> visited;
+
+    public boolean dfs(int node, int prev) {
+        if (visited.contains(node)) return false;
+
+        visited.add(node);
+        for (int n : adj.get(node)) {
+            if (n == prev) continue;
+            if (!dfs(n, node)) return false;
+        }
+
+        return true;
+    }
+    public boolean validTree(int n, int[][] edges) {
+        this.adj = new HashMap<>();
+        this.visited = new HashSet<>();
+        for (int i = 0; i < n; i ++) {
+            adj.put(i, new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        return dfs(0, -1) && visited.size() == n;
+    }
+}
