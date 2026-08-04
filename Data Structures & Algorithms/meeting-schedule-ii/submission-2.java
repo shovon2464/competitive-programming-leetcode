18ecae3343937cbf276a5d7c5ed7221872b65ace
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-08-04 17:47:32 -0600

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.java
@@ -0,0 +1,41 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        int n = intervals.size();
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for (int i = 0; i < n; i ++) {
+            start[i] = intervals.get(i).start;
+            end[i] = intervals.get(i).end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int count = 0;
+        int maxCount = 0;
+        int s = 0;
+        int e = 0;
+
+        while (s < start.length) {
+            if (start[s] < end[e]) {
+                count ++;
+                s ++;
+            } else {
+                e ++;
+                count --;
+            }
+            maxCount = Math.max(count, maxCount);
+        }
+
+        return maxCount;
+    }
+}
