eda0ff5e232530154857cd576ab265e48259f52b
Shovon Bhowmick <46128485+shovon2464@users.noreply.github.com>
2026-08-04 14:54:21 -0600

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.java
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals, (a,b) -> (a.start - b.start));
+
+        for (int i = 0; i < intervals.size() - 1; i ++) {
+            Interval i1 = intervals.get(i);
+            Interval i2 = intervals.get(i + 1);
+            if (i1.end > i2.start) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
